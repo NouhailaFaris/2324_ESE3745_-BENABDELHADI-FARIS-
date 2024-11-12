3c24d63f55436ec50f521f0bfb3235da95fdf672
MohamedBenabdelhadi <simobenabdelhadi89@gmail.com>
2024-11-12 21:45:36 +0100

    tp1

--- NUCLEO-G474RET6-Inverter_Pinout/Core/Src/stm32g4xx_hal_msp.c
+++ NUCLEO-G474RET6-Inverter_Pinout/Core/Src/stm32g4xx_hal_msp.c
@@ -82,5 +82,18 @@ void HAL_MspInit(void)
 }
 
 /* USER CODE BEGIN 1 */
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART2 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
 
+  return ch;
+}
 /* USER CODE END 1 */
